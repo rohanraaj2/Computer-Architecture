62c45f5e481e38523ea4086829291d36ed0a279c
Rohan Raj <rohan.raaj2@gmail.com>
2023-05-09 19:29:59 +0500

    Create 1.asm

---
+++ Lab 02/1.asm
@@ -0,0 +1,6 @@
+add x10, x1, x2 # b = 0 + 0;  
+add x3, x10, x4 # a = b + 3 2;  
+add x5, x10, x3 # c = a + b ;  
+sub x7, x5, x6 # d = c − 5; 
+add x8, x5, x7 # x = c + d
+add x9, x5, x8 # e = x + c since a + b = c, we c is used instead
