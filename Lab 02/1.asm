15bc4bbf051dc04a410b131f901bc119904262af
Rohan Raj <rohan.raaj2@gmail.com>
2023-05-09 19:30:50 +0500

    Update 1.asm

--- Lab 02/1.asm
+++ Lab 02/1.asm
@@ -4,3 +4,11 @@ add x5, x10, x3 # c = a + b ;
 sub x7, x5, x6 # d = c − 5; 
 add x8, x5, x7 # x = c + d
 add x9, x5, x8 # e = x + c since a + b = c, we c is used instead
+
+# Using addi:
+add x10, x1, x2 # b = 0 + 0;  
+addi x3, x10, 32 # a = b + 3 2;  
+add x5, x10, x3 # c = a + b ;  
+addi x7, x5, -5 # d = c − 5; 
+add x8, x5, x7 # x = c + d
+add x9, x5, x8 # e = x + c since a + b = c, we c is used instead
